990b0600e43e9a520dc5a9d5c9c673ac88df34c0
Ziad-I <68874104+Ziad-I@users.noreply.github.com>
2023-12-18 21:16:16 +0200

    fix batch file

--- run.bat
+++ run.bat
@@ -2,9 +2,9 @@
 
 rem Compile the C++ file using g++
 g++ -o code_gen code_gen.cpp
-.\output\code_gen
+.\code_gen
 g++ -o simulation simulation.cpp
-.\output\simulation
+.\simulation
 
 rem Pause to keep the command window open
 pause
