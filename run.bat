83ec0bd26842d820b6a5e9c0749bc698ee4b898a
Ziad-I <68874104+Ziad-I@users.noreply.github.com>
2023-12-18 22:09:37 +0200

    made the bat file stop if compilation failed
    made bat file stop if compilation of code_gen.cpp fails

--- run.bat
+++ run.bat
@@ -1,10 +1,16 @@
-
+@echo off
 
 rem Compile the C++ file using g++
 g++ -o code_gen code_gen.cpp
 .\code_gen
-g++ -o simulation simulation.cpp
-.\simulation
+
+if %errorlevel% equ 0 (
+    rem Run the compiled executable
+    g++ -o simulation simulation.cpp
+    .\simulation
+) else (
+    echo code_gen.cpp compilation failed.
+)
 
 rem Pause to keep the command window open
 pause
